7bbe4f2fae58455385a87ad559639c67825027de
kmarks2013 <kmarks2013@gmail.com>
2020-04-14 22:03:57 -0400

    adds button for start and stop

--- src/components/Metronome.js
+++ src/components/Metronome.js
@@ -10,6 +10,7 @@ export const Metronome = () => {
                 <h1>{bpm} BPM </h1>
                <input type='range' min='50' max="240" value={bpm} />
            </div>
+           <button>{playing ? "Stop" : "Start"} </button>
         </div>
     )
 }
